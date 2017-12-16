8f7aaa8d2ad71689b473b215ea9e539d601d5983
Armen <armen0089@gmail.com>
2017-12-16 13:27:59 +0400

    Added game page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-	public testData:string = null;	
+	public testData:string = null;
 
   constructor(public navCtrl: NavController) {
     this.testData = "Hello E Collors";
+  }
+
+  startGame () {
+      debugger;
   }
 
 }
